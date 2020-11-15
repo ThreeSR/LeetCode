1ae7d0b38c2f7ff88b80d1c3ed2a3a8f31fcedda
Three <36061421+ThreeSR@users.noreply.github.com>
2020-11-15 14:39:47 +0800

    Add files via upload

---
+++ LC263_Ugly Number_Naive Algorithm.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int num) {
+        if(num==0) return false;
+        while(true){
+            if(num==1) return true;
+            if(num%2==0) num/=2;
+            else if(num%3==0) num/=3;
+            else if(num%5==0) num/=5;
+            else return false;
+        }
+
+    }
+}
