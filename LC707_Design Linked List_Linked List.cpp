d141d056945234332779f08847144b4498dd4254
Three <36061421+ThreeSR@users.noreply.github.com>
2020-11-22 11:32:27 +0800

    Add files via upload

---
+++ LC707_Design Linked List_Linked List.cpp
@@ -0,0 +1,136 @@
+class MyLinkedList { // LC707，一道很适合学习链表的题目
+public:
+    // 定义链表节点结构体
+    struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
+
+    // 初始化链表
+    MyLinkedList() {
+        _dummyHead = new LinkedNode(0); // 这里定义的头结点 是一个虚拟头结点，而不是真正的链表头结点
+        _size = 0;
+    }
+
+    // 获取到第index个节点数值，如果index是非法数值直接返回-1， 注意index是从0开始的，第0个节点就是头结点
+    int get(int index) {
+        if (index > (_size - 1) || index < 0) {
+            return -1;
+        }
+        LinkedNode* cur = _dummyHead->next;
+        while(index--){ // 如果--index 就会陷入死循环
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+
+    // 在链表最前面插入一个节点，插入完成后，新插入的节点为链表的新的头结点
+    void addAtHead(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
+    }
+
+    // 在链表最后面添加一个节点
+    void addAtTail(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(cur->next != nullptr){
+            cur = cur->next;
+        }
+        cur->next = newNode;
+        _size++;
+    }
+
+    // 在第index个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+    // 如果index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+    // 如果index大于链表的长度，则返回空
+    void addAtIndex(int index, int val) {
+        if (index > _size) {
+            return;
+        }
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
+    }
+
+    // 删除第index个节点，如果index 大于等于链表的长度，直接return，注意index是从0开始的
+    void deleteAtIndex(int index) {
+        if (index >= _size || index < 0) {
+            return;
+        }
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur ->next;
+        }
+        LinkedNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        _size--;
+    }
+
+    // 打印链表
+    void printLinkedList() {
+        LinkedNode* cur = _dummyHead;
+        while (cur->next != nullptr) {
+            cout << cur->next->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+private:
+    int _size;
+    LinkedNode* _dummyHead;
+
+};
+
+
+// class MyLinkedList {
+// public:
+//     /** Initialize your data structure here. */
+//     MyLinkedList() {
+
+//     }
+    
+//     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+//     int get(int index) {
+
+//     }
+    
+//     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+//     void addAtHead(int val) {
+
+//     }
+    
+//     /** Append a node of value val to the last element of the linked list. */
+//     void addAtTail(int val) {
+
+//     }
+    
+//     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+//     void addAtIndex(int index, int val) {
+
+//     }
+    
+//     /** Delete the index-th node in the linked list, if the index is valid. */
+//     void deleteAtIndex(int index) {
+
+//     }
+// };
+
+// /**
+//  * Your MyLinkedList object will be instantiated and called as such:
+//  * MyLinkedList* obj = new MyLinkedList();
+//  * int param_1 = obj->get(index);
+//  * obj->addAtHead(val);
+//  * obj->addAtTail(val);
+//  * obj->addAtIndex(index,val);
+//  * obj->deleteAtIndex(index);
+//  */
