6724772565faec244f1413a94a9282002cf66bae
Three <36061421+ThreeSR@users.noreply.github.com>
2020-11-30 11:43:45 +0800

    Add files via upload

---
+++ LC337_House Robber III_DP.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution { // 后序遍历+DP
+public:
+    
+    unordered_map<TreeNode*, int> f, g; // 构建哈希表
+    
+    void dfs(TreeNode* root){ // 深度优先搜索，后序遍历。树形题目在转化过程中常用后序遍历
+        if (root == nullptr) return; // 特判
+        dfs(root->left); // 后序遍历
+        dfs(root->right);
+        f[root] = root->val + g[root->left] + g[root->right]; // f代表选当前节点，g代表不选当前节点；用f代表它的两个子节点不能用，用g代表子节点可用可不用
+        g[root] = max(f[root->left], g[root->left]) + max(f[root->right], g[root->right]); // 因为g代表子节点可用可不用，所以是子节点上f与g求max
+    }
+
+    int rob(TreeNode* root) {
+        dfs(root); // 先遍历求f与g
+        return max(f[root],g[root]); // 得到最大值
+    }
+};
+
+// reference https://leetcode-cn.com/problems/house-robber-iii/solution/da-jia-jie-she-iii-by-leetcode-solution/ 详见对应题解以及Solution中内容
