da1b9cfec32ffdb57ecf01bacd97d1bf9f8d90a3
Three <36061421+ThreeSR@users.noreply.github.com>
2020-12-01 15:16:43 +0800

    Add files via upload

---
+++ LC133_Clone Graph_Graph.py
@@ -0,0 +1,11 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        return deepcopy(node)
