c6f351cd5bb405d728ca2cca0a441547cbc98a82
Three <36061421+ThreeSR@users.noreply.github.com>
2020-12-07 15:03:15 +0800

    Add files via upload

---
+++ LC105_Construct Binary Tree from Preorder and Inorder Traversal_Recursion.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {  // 本题可以参考LC106
+private:
+        TreeNode* traversal (vector<int>& inorder, int inorderBegin, int inorderEnd, vector<int>& preorder, int preorderBegin, int preorderEnd) {
+        if (preorderBegin == preorderEnd) return NULL;
+
+        int rootValue = preorder[preorderBegin]; // 注意用preorderBegin 不要用0
+        TreeNode* root = new TreeNode(rootValue);
+
+        if (preorderEnd - preorderBegin == 1) return root;
+
+        int delimiterIndex;
+        for (delimiterIndex = inorderBegin; delimiterIndex < inorderEnd; delimiterIndex++) {
+            if (inorder[delimiterIndex] == rootValue) break;
+        }
+        // 切割中序数组
+        // 中序左区间，左闭右开[leftInorderBegin, leftInorderEnd)
+        int leftInorderBegin = inorderBegin;
+        int leftInorderEnd = delimiterIndex;
+        // 中序右区间，左闭右开[rightInorderBegin, rightInorderEnd)
+        int rightInorderBegin = delimiterIndex + 1;
+        int rightInorderEnd = inorderEnd;
+        
+        // 切割前序数组
+        // 前序左区间，左闭右开[leftPreorderBegin, leftPreorderEnd)
+        int leftPreorderBegin =  preorderBegin + 1;
+        int leftPreorderEnd = preorderBegin + 1 + delimiterIndex - inorderBegin; // 终止位置是起始位置加上中序左区间的大小size
+        // 前序右区间, 左闭右开[rightPreorderBegin, rightPreorderEnd)
+        int rightPreorderBegin = preorderBegin + 1 + (delimiterIndex - inorderBegin);
+        int rightPreorderEnd = preorderEnd; 
+
+        root->left = traversal(inorder, leftInorderBegin, leftInorderEnd,  preorder, leftPreorderBegin, leftPreorderEnd);
+        root->right = traversal(inorder, rightInorderBegin, rightInorderEnd, preorder, rightPreorderBegin, rightPreorderEnd);
+
+        return root;
+    }
+
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (inorder.size() == 0 || preorder.size() == 0) return NULL;
+
+        // 参数坚持左闭右开的原则
+        return traversal(inorder, 0, inorder.size(), preorder, 0, preorder.size());
+    }
+};
+
+// reference https://mp.weixin.qq.com/s/7r66ap2s-shvVvlZxo59xg
