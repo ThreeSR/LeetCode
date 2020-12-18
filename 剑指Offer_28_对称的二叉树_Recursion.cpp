802958ee15f071f146167069fabfa03a58870fea
Three <36061421+ThreeSR@users.noreply.github.com>
2020-12-18 10:06:12 +0800

    Add files via upload

---
+++ 剑指Offer_28_对称的二叉树_Recursion.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* p, TreeNode* q) { // 递归
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
+
+// 同LC101
