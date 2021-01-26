b9809a407f78f84da91b797843fe914042a2eadb
Three <36061421+ThreeSR@users.noreply.github.com>
2021-01-26 11:58:34 +0800

    Add files via upload

---
+++ LC88_Merge Sorted Array_Double Pointer.py
@@ -0,0 +1,36 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Approach 1
+        #nums1[:] = sorted(nums1[:m] + nums2) # you can use function directly
+        # but you do not fully utilize the condition in the question
+        
+        # Naturally, we want to use double pointer to solve this problem when we see it 
+        # Approach 2
+        # Make a copy of nums1.
+        nums1_copy = nums1[:m] 
+        nums1[:] = []
+
+        # Two get pointers for nums1_copy and nums2.
+        p1 = 0 
+        p2 = 0
+        
+        # Compare elements from nums1_copy and nums2
+        # and add the smallest one into nums1.
+        while p1 < m and p2 < n: 
+            if nums1_copy[p1] < nums2[p2]: 
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+
+        # if there are still elements to add
+        if p1 < m: 
+            nums1[p1 + p2:] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p1 + p2:] = nums2[p2:]
+
+# Notice: you still need to add elements if p1 < m or p2 < n
