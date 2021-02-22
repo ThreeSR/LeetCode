94e883a6803aa07e56771d26452c57f5ef342996
Three <36061421+ThreeSR@users.noreply.github.com>
2021-02-22 14:34:39 +0800

    Update LC198_House Robber_DP.cpp

--- LC198_House Robber_DP.cpp
+++ LC198_House Robber_DP.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-    if (nums.size() == 0) {
-        return 0;
-    }
-
-    int N = nums.size();
-    vector<int> dp(N+1, 0);
-    dp[0] = 0;
-    dp[1] = nums[0];
-    for (int k = 2; k <= N; k++) {
-        dp[k] = max(dp[k-1], nums[k-1] + dp[k-2]);
-    }
-    return dp[N];
+        if (nums.size() == 0) return 0; // 特判
+        if (nums.size() == 1) return nums[0];
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]); // 第一次进行选择地偷盗
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]); // 状态转移
+        }
+        return dp[nums.size() - 1];
     }
 };
+
+// reference https://mp.weixin.qq.com/s/UZ31WdLEEFmBegdgLkJ8Dw
