d86a877b29ff2c2499fa55816eef49d0cf38b9c6
Three <36061421+ThreeSR@users.noreply.github.com>
2021-03-06 11:10:18 +0800

    Add files via upload

---
+++ 剑指Offer_29_顺时针打印矩阵_Naive Algorithm.py
@@ -0,0 +1,19 @@
+class Solution:
+    def spiralOrder(self, matrix:[[int]]) -> [int]: # 本题和LC54螺旋矩阵一样，仔细按照题目要求打印出矩阵即可。
+        if not matrix: return []
+        l, r, t, b, res = 0, len(matrix[0]) - 1, 0, len(matrix) - 1, []
+        while True:
+            for i in range(l, r + 1): res.append(matrix[t][i]) # left to right
+            t += 1
+            if t > b: break
+            for i in range(t, b + 1): res.append(matrix[i][r]) # top to bottom
+            r -= 1
+            if l > r: break
+            for i in range(r, l - 1, -1): res.append(matrix[b][i]) # right to left
+            b -= 1
+            if t > b: break
+            for i in range(b, t - 1, -1): res.append(matrix[i][l]) # bottom to top
+            l += 1
+            if l > r: break
+        return res
+# reference https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/mian-shi-ti-29-shun-shi-zhen-da-yin-ju-zhen-she-di/
