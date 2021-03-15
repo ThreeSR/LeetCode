38f6ee2bba8240dcee4fdd40de6eddbef823398f
Three <36061421+ThreeSR@users.noreply.github.com>
2021-03-15 20:32:35 +0800

    Add files via upload

---
+++ LC343_Integer Break_Math.py
@@ -0,0 +1,9 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3: return n - 1
+        a, b = n // 3, n % 3
+        if b == 0: return int(math.pow(3, a))
+        if b == 1: return int(math.pow(3, a - 1) * 4)
+        return int(math.pow(3, a) * 2)
+
+# 解答详见剑指offer14-I，两题一致。
